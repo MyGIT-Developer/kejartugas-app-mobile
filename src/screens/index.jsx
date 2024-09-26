20acc2291f97b787eb3742f72b0ea5f0830f5cf8
Dhanuwardhan <danu.wardhana@innovation.co.id>
2024-09-26 16:31:08 +0700

    fix new

--- src/screens/index.jsx
+++
@@ -1,8 +0,0 @@
-import Home from "./Home"
-import Kehadiran from "./Kehadiran"
-import Project from "./Project"
-import Tugas from "./Tugas"
-import Profile from "./Profile"
-import ColorList from '../../components/ColorList'
-
-export { Home, Kehadiran, Project, Tugas, Profile } 
